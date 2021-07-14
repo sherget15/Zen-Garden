99dafa91aba1e78660184e0e92b625c25fdc1106
moos <herget06@gmail.com>
2021-07-14 15:57:26 -0400

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Route exact path="/">Zen Garden</Route>
+      <Route exact path="/home">Recipes</Route>
+      <Route path="/new-plant"></Route>
+      <Footer />
     </div>
   );
 }
