93bd433f06c1031720efff95d32123e6e8edbf9c
moos <herget06@gmail.com>
2021-07-20 05:38:21 -0400

    12

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,14 +4,28 @@ import { Link } from 'react-router-dom';
 export default function NavBar() {
   return (
     <div className="nav-main">
+
       <div className="nav-left">
         <img className="logo" src="https://cdn5.vectorstock.com/i/1000x1000/49/44/yoga-lotus-with-zen-circle-logo-template-vector-15844944.jpg" alt="zen-circle" />
-        <Link to="/">Zen Garden</Link>
+        <Link to="/" className="Zen-Garden">Zen Garden</Link>
+      </div>
+
+      <div className="left-name">
       </div>
 
       <div className="nav-right">
-        <Link className="nav-link" to="/new-plant">Share</Link>
-        <Link className="nav-link" to="/about">About</Link>
+        <ul>
+
+          <Link
+            className="Share" to="/new-plant"
+          >Share</Link>
+
+          <Link
+            className="About"
+            to="/about"
+          >About</Link>
+
+        </ul>
       </div>
     </div>
 
