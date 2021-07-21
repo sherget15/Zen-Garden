5ec3ac8cab9dc9fe6515662edf2af9be63cb5c9d
moos <herget06@gmail.com>
2021-07-21 14:17:59 -0400

    cleaning out comments

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,20 +24,15 @@ export default function Home() {
           <div className='card-spacing'>
             <div className='card'
               style={{
-                // backgroundImage: "url(https://cdn.shopify.com/s/files/1/2719/1640/products/03dab051c61960236827063d4282c73a_medium.jpg?v=1581703782)",
-                // height: "300px",
                 backgroundImage: `url("${plant.fields.plantImage}")`,
                 backgroundSize: "100%",
                 backgroundPosition: "center",
                 backgroundRepeat: "no-repeat"
               }} >
-              {/* <Link to={`/plants/${plant.id}`} key={plant.id}> */}
               < h3 > {plant.fields.plantName}</h3>
               <p>{plant.fields.plantLocation}</p>
-              {/* <img className='card-image' src={plant.fields.plantImage} alt={plant.fields.name} /> */}
               <p>{plant.fields.plantDescription}</p>
               <p>{plant.fields.userName}</p>
-              {/* </Link> */}
             </div>
           </div>
         );
